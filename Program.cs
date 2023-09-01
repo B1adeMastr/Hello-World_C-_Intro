3cf98cc5a1f71d106d65eccb9308038e81995672
B1adeMastr <93338755+B1adeMastr@users.noreply.github.com>
2023-08-31 19:20:16 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hello_World
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Hello World!");
+
+        }
+
+    }
+
+}
